dd15d8a33d69ac9b2c96ad34a1cfccdff09e5b37
alexander.palisoc <alexander.palisoc@fdmgroup.com>
2019-03-19 11:43:33 -0400

    Created the Group model class and the DAO to create a group, find by group name or ID and update group

---
+++ src/com/fdmgroup/DAO/GroupDAO.java
@@ -0,0 +1,107 @@
+package com.fdmgroup.DAO;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import com.fdmgroup.model.Group;
+
+
+/**
+ * 
+ * GroupDAO class used to access and modify the table for Group.
+ */
+public class GroupDAO {
+	
+	/**
+	 * Single instance of the DBConnection
+	 */
+	private DBConnection conn;
+
+	public GroupDAO() {
+		super();
+		conn = DBConnection.getInstance();
+	}
+	
+	/**
+	 * 
+	 * Create is called when user wants to persist an entity of type group in the database.
+	 * 
+	 * @param g: Group that wants to be saved in database
+	 * @return Group that was saved.
+	 */
+	public Group create(Group g){
+		EntityManager em = conn.getEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(g);
+		em.getTransaction().commit();
+		em.close();
+		return g;
+	}
+	
+	/**
+	 * 
+	 * findByGroupId uses the named query to find the group with the given group id.
+	 * 
+	 * @param id: Id of the group you would like to find.
+	 * @return Group with the corresponding id.
+	 */
+	public Group findByGroupId(long id){
+		EntityManager em = conn.getEntityManager();
+		TypedQuery<Group> query = em.createNamedQuery("group.findByGroupId", Group.class);
+		query.setParameter("gId", id);
+		
+		List<Group> groups = query.getResultList();
+		if(groups != null && !groups.isEmpty()){
+			return groups.get(0);
+		} else {
+			return null;
+		}
+	}
+	/**
+	 * 
+	 * findByGroupName uses the named query to find the group with the given group name.
+	 * 
+	 * @param name: Name of the group you would like to find.
+	 * @return Group with the corresponding name.
+	 */
+	public Group findByGroupName(String name){
+		EntityManager em = conn.getEntityManager();
+		TypedQuery<Group> query = em.createNamedQuery("group.findByGroupName", Group.class);
+		query.setParameter("gName", name);
+		
+		List<Group> groups = query.getResultList();
+		if(groups != null && !groups.isEmpty()){
+			return groups.get(0);
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * 
+	 * update finds the group you want to update in the database and updates the group to the fields in given group.
+	 * 
+	 * @param group: The group with updates to it.
+	 */
+	public void update(Group group){
+		EntityManager em = conn.getEntityManager();
+		Group foundGroup = em.find(Group.class, group.getGroupId());
+		
+		em.getTransaction().begin();
+		foundGroup.setActive(group.isActive());
+		foundGroup.setGroupCategory(group.getGroupCategory());
+		foundGroup.setGroupDescription(group.getGroupDescription());
+		foundGroup.setGroupName(group.getGroupName());
+		em.getTransaction().commit();
+		
+		em.close();
+	}
+	
+	
+	
+	
+	
+}
--- src/com/fdmgroup/model/Group.java
+++ src/com/fdmgroup/model/Group.java
@@ -1,5 +1,128 @@
 package com.fdmgroup.model;
 
-public class Group {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+import com.fdmgroup.enums.Category;
+
+//TODO: Check if group can get created in oracle.
+/**
+ * Group is the model class that holds the information of the groups that users can join. 
+ *
+ */
+@Entity
+@Table(name="USER_GROUP")
+@NamedQueries({
+	@NamedQuery(name="group.findByGroupId", query="SELECT g FROM Group g WHERE g.groupId = :gId"),
+	@NamedQuery(name="group.findByGroupName", query="SELECT g FROM Group g WHERE g.groupName = :gName")
+})
+public class Group {
+	/**
+	 * The unique identifier for Group class. 
+	 */
+	@Id
+	@SequenceGenerator(name="groupSeq", sequenceName="GROUP_SEQ", initialValue=1, allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="groupSeq")
+	@Column(name="USER_GROUP_ID")
+	private long groupId;
+	
+	/**
+	 * The name the group is called in the application.
+	 */
+	@Column(name="GROUP_NAME")
+	private String groupName;
+	
+	/**
+	 * The category the group is part of.
+	 */
+	@Column(name="GROUP_CATEGORY")
+	private Category groupCategory;
+	
+	/**
+	 * The description of the group.
+	 */
+	@Column(name="GROUP_DESCRIPTION")
+	private String groupDescription;
+	
+	/**
+	 * If the group is active or deactive
+	 */
+	@Column(name="ACTIVE", columnDefinition="Number(1)")
+	private boolean isActive;
+	
+	/**
+	 * List of Users currently in the group.
+	 */
+	//@OneToMany(mappedBy="group")
+	//@Column(name=GROUP_MEMBERS)
+	//private List<IRUser> groupMembers;
+	
+	public Group() {
+		super();
+	}
+	
+	public Group(String groupName, Category groupCategory, String groupDescription) {
+		super();
+		this.groupName = groupName;
+		this.groupCategory = groupCategory;
+		this.groupDescription = groupDescription;
+		this.isActive = true;
+		// this.groupMembers = new ArrayList<>();
+	}
+	
+	public String getGroupName() {
+		return groupName;
+	}
+	
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+	
+	public Category getGroupCategory() {
+		return groupCategory;
+	}
+	
+	public void setGroupCategory(Category groupCategory) {
+		this.groupCategory = groupCategory;
+	}
+	
+	public String getGroupDescription() {
+		return groupDescription;
+	}
+	
+	public void setGroupDescription(String groupDescription) {
+		this.groupDescription = groupDescription;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public long getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(long groupId) {
+		this.groupId = groupId;
+	}
+
+	@Override
+	public String toString() {
+		return "Group [groupId=" + groupId + ", groupName=" + groupName + ", groupCategory=" + groupCategory
+				+ ", groupDescription=" + groupDescription + ", isActive=" + isActive + "]";
+	}
+	
+	
+	
 }
