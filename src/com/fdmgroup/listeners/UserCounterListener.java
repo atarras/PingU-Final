49ca4d14a9835f7fd6b76f214eab83441ab8913f
rajat.goyal <rajat.goyal@fdmgroup.com>
2019-03-28 16:21:00 -0400

    fixed online user issue

--- src/com/fdmgroup/listeners/UserCounterListener.java
+++ src/com/fdmgroup/listeners/UserCounterListener.java
@@ -1,8 +1,6 @@
 package com.fdmgroup.listeners;
 
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 import javax.servlet.annotation.WebListener;
@@ -14,58 +12,23 @@ import com.fdmgroup.model.IUser;
 @WebListener
 public class UserCounterListener implements HttpSessionAttributeListener {
 
-//	private Vector<IUser> users = new Vector<>();
-//	private Vector<IUser> newUsers = new Vector<>();
-	
-	static private  Map<Long,IUser> users = new HashMap<>();
-	
-//	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	static private Map<Long, IUser> users = new HashMap<>();
 
 	public void attributeAdded(HttpSessionBindingEvent e) {
-		
-//		if (principal instanceof IUser) {
-//			  String username = ((IUser)principal).getUsername();
-//			} else {
-//			  String username = principal.toString();
-//			}
-		
+
 		IUser newLoggedInUser = (IUser) e.getValue();
 		if (e.getName().equals("newUser")) {
-//			if(loggedUsers.size()<1){
 			users.put(newLoggedInUser.getUserId(), newLoggedInUser);
-//				users.add((IUser) e.getValue());
-				System.out.println("USER HAS BEEN ADDED TO users");
-				e.getSession().getServletContext().setAttribute("loggedInUsers", users);
-				return;
-//			}
-//			Iterator it = loggedUsers.entrySet().iterator();
-//		    while (it.hasNext()) {
-//		        Map.Entry pair = (Map.Entry)it.next();
-//		        System.out.println(pair.getKey() + " = " + pair.getValue());
-//		        it.remove(); // avoids a ConcurrentModificationException
-//		    }
-//			}
-//			Collections.sort(users);
+			System.out.println("USER HAS BEEN ADDED TO users");
+			e.getSession().getServletContext().setAttribute("loggedInUsers", users);
+			return;
 		}
 
-		// if (e.getName().equals("newUser") && !users.contains(e.getValue())){
-		// users.add((IUser)e.getValue());
-		// System.out.println("USER HAS BEEN ADDED TO users");
-		// e.getSession().getServletContext().setAttribute("loggedInUsers",
-		// users);
-		// }
 	}
 
 	public void attributeRemoved(HttpSessionBindingEvent e) {
 		IUser loggedOutUser = (IUser) e.getValue();
 		if (e.getName().equals("newUser")) {
-//			for (IUser iUser : users) {
-//				if (loggedOutUser.getUserId()!=iUser.getUserId())
-//					newUsers.add(iUser);
-//			}
-			// users.remove((IUser)e.getValue());
-//			users = newUsers;
-//			Collections.sort(users);
 			users.remove(loggedOutUser.getUserId());
 			System.out.println("USER HAS BEEN REMOVED TO users");
 			e.getSession().getServletContext().setAttribute("loggedInUsers", users);
@@ -73,6 +36,5 @@ public class UserCounterListener implements HttpSessionAttributeListener {
 	}
 
 	public void attributeReplaced(HttpSessionBindingEvent arg0) {
-		// TODO Auto-generated method stub
 	}
 }
