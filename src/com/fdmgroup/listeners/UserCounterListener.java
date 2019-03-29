159f0864451c5f5fa07101dce503149d6fbe9d59
rajat.goyal <rajat.goyal@fdmgroup.com>
2019-03-29 15:47:14 -0400

    fixed admin login issue

--- src/com/fdmgroup/listeners/UserCounterListener.java
+++ src/com/fdmgroup/listeners/UserCounterListener.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionAttributeListener;
 import javax.servlet.http.HttpSessionBindingEvent;
 
+import com.fdmgroup.model.Admin;
 import com.fdmgroup.model.IUser;
 
 @WebListener
@@ -17,6 +18,8 @@ public class UserCounterListener implements HttpSessionAttributeListener {
 	public void attributeAdded(HttpSessionBindingEvent e) {
 
 		IUser newLoggedInUser = (IUser) e.getValue();
+		if(newLoggedInUser instanceof Admin)
+			return;
 		if (e.getName().equals("newUser")) {
 			users.put(newLoggedInUser.getUserId(), newLoggedInUser);
 			System.out.println("USER HAS BEEN ADDED TO users");
@@ -28,6 +31,8 @@ public class UserCounterListener implements HttpSessionAttributeListener {
 
 	public void attributeRemoved(HttpSessionBindingEvent e) {
 		IUser loggedOutUser = (IUser) e.getValue();
+		if(loggedOutUser instanceof Admin)
+			return;
 		if (e.getName().equals("newUser")) {
 			users.remove(loggedOutUser.getUserId());
 			System.out.println("USER HAS BEEN REMOVED TO users");
